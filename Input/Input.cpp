3169e502ab6c3bc7d3c9ca7f3c56313114b4c552
SifatNiloy <sifatniloy18@gmail.com>
2023-01-29 11:01:30 -0800

    different type input from user

---
+++ Input/Input.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+
+    // int num;
+    // cout<<"enter an integer number: ";
+    // cin>>num;
+    // cout<<"entered number is "<<num<<endl;
+
+
+    // char ch;
+    // cout<<"enter a character : ";
+    // cin>>ch;
+    // cout<<"entered character is :"<<ch;
+    
+
+    // char name[20];
+    // cout<<"enter your name : ";
+    // cin>>name;
+    // cout<<"Welcome :"<<name;
+
+    int num1, num2, sum;
+    cout<<"Enter two integer numbers: ";
+    cin>>num1>>num2;
+
+    sum= num1+num2;
+    cout<<"sum of the two numbers is= " <<sum;
+
+    // getch();
+}
---
+++ Input/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+    // cout<<"enter an integer number: ";
+    // cin>>num;
+    // cout<<"entered number is "<<num<<endl;
+
+
+    // char ch;
+    // cout<<"enter a character : ";
+    // cin>>ch;
+    // cout<<"entered character is :"<<ch;
