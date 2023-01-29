6a7795112c0d03315dd9120c04b5bdfd82cdbd54
SifatNiloy <sifatniloy18@gmail.com>
2023-01-29 11:19:58 -0800

    operator

---
+++ Operator/Operator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    // num1=10;
+    // num2=20;
+    cout<<"Enter two numbers: ";
+    cin>>num1>>num2;
+    int sum= num1+num2;
+    cout<<"sum is= "<<sum<<endl;
+    int sub= num1-num2;
+    cout<<"subtraction is= "<<sub<<endl;
+    int multiplication= num1*num2;
+    cout<<"multiplication is= "<<multiplication<<endl;
+    int div= num1/num2;
+    cout<<"divition is= "<<div;
+}
