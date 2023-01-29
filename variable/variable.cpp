97eb5516bad75e5ca8002726fd430a41e8ab8f8d
SifatNiloy <sifatniloy18@gmail.com>
2023-01-29 10:34:39 -0800

    'variables'

---
+++ variable/variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+    char ch;
+    ch='a';
+    double num3=10.56;
+    float num4=10.5323656;
+
+    num1=10;
+    num2=20;
+    cout<<"number 1= " <<num1<<endl <<"nubmer 2= " <<num2<<endl;
+    cout<<"character= "<<ch<<endl;
+    cout<<"double_number= "<<num3<<endl;
+    cout<<"float_number= "<<num4; 
+}
