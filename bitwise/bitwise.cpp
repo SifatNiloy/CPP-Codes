f05fa93c9f59f4f6f428f6be218cb323d134d005
SifatNiloy <sifatniloy18@gmail.com>
2023-01-29 15:55:32 -0800

    bitwise and assignment operator

---
+++ bitwise/bitwise.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=13;
+    int b=22;
+    int c;
+
+    c=a&b;
+    cout<<c<<endl;
+    c=a|b;
+    cout<<c<<endl;
+    c=a^b;
+    cout<<c<<endl;
+
+}
