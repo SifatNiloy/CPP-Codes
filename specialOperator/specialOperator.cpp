0f997901540348f78eaff72753c0ffa02871d586
SifatNiloy <sifatniloy18@gmail.com>
2023-01-30 11:00:48 -0800

    opertor sizeof()

---
+++ specialOperator/specialOperator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    float f;
+    double d;
+    char ch;
+    char name[20];
+
+    int c= sizeof(d);
+    cout<<c;
+
+    
+
+
+}
