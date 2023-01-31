b508101e0786b592a213edb1991e66cc002bd6ca
SifatNiloy <sifatniloy18@gmail.com>
2023-01-31 11:05:33 -0800

    matrix array input

--- 2Darray/2Darray.cpp
+++ 2Darray/2Darray.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    int A[2][3]={
-        {10,20,30},
-        {40,50,60}
-    };
+    int A[2][3];
+    cout<<"Enter the elements for 2D array \n";
     for (int row=0; row<2; row++){
         for (int col=0; col<3; col++){
-            cout<<A[row][col]<<" ";
+            cout<<"A["<<row<<"]["<<col<<"] = ";
+            cin>>A[row][col];
         }
         cout << endl;
     }
 }
+
